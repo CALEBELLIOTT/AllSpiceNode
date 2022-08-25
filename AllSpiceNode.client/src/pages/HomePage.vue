37750c237cb62478d990549c378dc3c1db95e4ab
Caleb Elliott <caleb123elliott@gmail.com>
2022-08-25 12:35:40 -0600

    front end changes

--- AllSpiceNode.client/src/pages/HomePage.vue
+++ AllSpiceNode.client/src/pages/HomePage.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <!-- <div class="hero-img">
+  </div> -->
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="bg-light rounded p-2 mt-md-5 mt-3">
+          <h4 class="text-center text-primary">Our Recent Favorites</h4>
+
+        </div>
+      </div>
     </div>
   </div>
+
+
 </template>
 
 <script>
@@ -16,21 +23,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-  .home-card{
-    width: 50vw;
-    > img{
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
+@import "../assets/scss/variables";
+
+// .hero-img {
+//   background-image: url(https://i.imgur.com/VYyBKmi.jpg);
+//   border-top: 2px solid $primary;
+//   height: 75vh;
+//   width: 100vw;
+//   background-size: cover;
+//   background-repeat: no-repeat;
+
+// }
 </style>
