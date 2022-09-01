ff5cca02c6dd21789557402ed535ec90dc20799b
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-01 12:12:30 -0600

    build

--- AllSpiceNode.client/vite.config.js
+++ AllSpiceNode.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../AllSpiceNode/client',
     sourcemap: false
   },
   server: {
