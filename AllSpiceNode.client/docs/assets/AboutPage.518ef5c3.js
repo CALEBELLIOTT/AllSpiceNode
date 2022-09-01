22c2596880eceae201f1d321d00cab60addbee2c
Caleb Elliott <caleb123elliott@gmail.com>
2022-09-01 11:58:29 -0600

    build

---
+++ AllSpiceNode.client/docs/assets/AboutPage.518ef5c3.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,a as s}from"./index.28fefcaa.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),_=[n];function r(i,d,p,u,h,l){return o(),t("div",c,_)}const m=e(a,[["render",r]]);export{m as default};
